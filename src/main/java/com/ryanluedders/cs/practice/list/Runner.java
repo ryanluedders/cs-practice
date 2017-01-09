907610d7b5c4c49fed9eb7a6c0e164ee10d031db
Ryan Luedders <ryan@ryanluedders.com>
2017-01-08 21:37:34 -0600

    initial commit to cs-practice repo

---
+++ src/main/java/com/ryanluedders/cs/practice/list/Runner.java
@@ -0,0 +1,11 @@
+package com.ryanluedders.cs.practice.list;
+
+import java.io.IOException;
+
+public class Runner {
+    
+    public static void main(String[] argvs) throws IOException {
+        
+    }
+    
+}
---
+++ src/test/java/com/ryanluedders/cs/practice/list/RunnerTest.java
@@ -0,0 +1,5 @@
+package com.ryanluedders.cs.practice.list;
+
+public class RunnerTest {
+
+}
