c6653cd1325ce66480bccc37a8997b9e86aa1ea7
Ryan Luedders <ryan.luedders@gmail.com>
2017-01-08 22:59:58 -0600

    change tabs to spaces (and test change of committer email address.)

--- src/main/java/com/ryanluedders/cs/practice/tree/BinaryTree.java
+++ src/main/java/com/ryanluedders/cs/practice/tree/BinaryTree.java
@@ -1,81 +1,81 @@
 package com.ryanluedders.cs.practice.tree;
 
 public class BinaryTree<T> {
-	
-	private Node<T> root = null;
-	
-	public class Node<NT> {
-		
-		NT value;
-		
-		Node<NT> left = null;
-		
-		Node<NT> right = null;
+    
+    private Node<T> root = null;
+    
+    public class Node<NT> {
+        
+        NT value;
+        
+        Node<NT> left = null;
+        
+        Node<NT> right = null;
 
-		public NT getValue() {
-			return value;
-		}
+        public NT getValue() {
+            return value;
+        }
 
-		public void setValue(NT value) {
-			this.value = value;
-		}
+        public void setValue(NT value) {
+            this.value = value;
+        }
 
-		public Node<NT> getLeft() {
-			return left;
-		}
+        public Node<NT> getLeft() {
+            return left;
+        }
 
-		public void setLeft(Node<NT> left) {
-			this.left = left;
-		}
+        public void setLeft(Node<NT> left) {
+            this.left = left;
+        }
 
-		public Node<NT> getRight() {
-			return right;
-		}
+        public Node<NT> getRight() {
+            return right;
+        }
 
-		public void setRight(Node<NT> right) {
-			this.right = right;
-		}
+        public void setRight(Node<NT> right) {
+            this.right = right;
+        }
 
-	}
-	
-	void insert(T value) {
-		root = insertOnNode(root, value);
-	}
-	
-	Node<T> insertOnNode(Node<T> root, T value) {
-		if (root == null) {
-			root = new Node<T>();
-			root.setValue(value);
-		} else if (value.hashCode() < root.getValue().hashCode()) {
-			root.setLeft(insertOnNode(root.getLeft(), value));
-		} else {
-			root.setRight(insertOnNode(root.getRight(), value));
-		}
-		return root;
-	}
-	
-	public String toString() {
-		return toString(root);
-	}
-	
-	String toString(Node<T> root) {
-		String result = "";
-		if (root == null) {
-			return "null";
-		}
-		
-		result += "{" + nodeToString(root) + "->" + "(" 
-				+ toString(root.getLeft()) + "," + toString(root.getRight()) + ")}";
-		return result;
-	}
-	
-	String nodeToString(Node<T> n) {
-		if (n == null) {
-			return "null";
-		} if (n.getValue() == null) {
-			return "value=null";
-		}
-		return String.valueOf(n.getValue());
-	}
+    }
+    
+    void insert(T value) {
+        root = insertOnNode(root, value);
+    }
+    
+    Node<T> insertOnNode(Node<T> root, T value) {
+        if (root == null) {
+            root = new Node<T>();
+            root.setValue(value);
+        } else if (value.hashCode() < root.getValue().hashCode()) {
+            root.setLeft(insertOnNode(root.getLeft(), value));
+        } else {
+            root.setRight(insertOnNode(root.getRight(), value));
+        }
+        return root;
+    }
+    
+    public String toString() {
+        return toString(root);
+    }
+    
+    String toString(Node<T> root) {
+        String result = "";
+        if (root == null) {
+            return "null";
+        }
+        
+        result += "{" + nodeToString(root) + "->" + "(" 
+                + toString(root.getLeft()) + "," + toString(root.getRight()) + ")}";
+        return result;
+    }
+    
+    String nodeToString(Node<T> n) {
+        if (n == null) {
+            return "null";
+        } if (n.getValue() == null) {
+            return "value=null";
+        }
+        return String.valueOf(n.getValue());
+    }
 
 }
--- src/test/java/com/ryanluedders/cs/practice/tree/BinaryTreeTest.java
+++ src/test/java/com/ryanluedders/cs/practice/tree/BinaryTreeTest.java
@@ -5,31 +5,31 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class BinaryTreeTest {
-	
-	@Test
-	public void testBinaryTree_toString_happy_path() {
-		BinaryTree<String> t = new BinaryTree<>();
-		t.insert("D");
-		t.insert("C");
-		t.insert("E");
-		t.insert("F");
-		
-		String result = t.toString();
-		
-		System.out.println(result);
-		
-		assertEquals("{D->({C->(null,null)},{E->(null,{F->(null,null)})})}", result);
-	}
-	
-	@Test
-	public void testBinaryTree_empty() {
-		BinaryTree<String> t = new BinaryTree<>();
-		
-		String result = t.toString();
-		
-		System.out.println(result);
-		
-		assertEquals("null", result);
-	}
-	
+    
+    @Test
+    public void testBinaryTree_toString_happy_path() {
+        BinaryTree<String> t = new BinaryTree<>();
+        t.insert("D");
+        t.insert("C");
+        t.insert("E");
+        t.insert("F");
+        
+        String result = t.toString();
+        
+        System.out.println(result);
+        
+        assertEquals("{D->({C->(null,null)},{E->(null,{F->(null,null)})})}", result);
+    }
+    
+    @Test
+    public void testBinaryTree_empty() {
+        BinaryTree<String> t = new BinaryTree<>();
+        
+        String result = t.toString();
+        
+        System.out.println(result);
+        
+        assertEquals("null", result);
+    }
+    
 }
